c97295ee2066378e2ef325f494864bba8b5411a8
Richard timothee Kanmadozo adjibade <rkanmado@3-F-6.42heilbronn.de>
2022-09-23 21:11:38 +0200

    Add the first mockup of the stack a sorting

--- shared/operations.c
+++ shared/operations.c
@@ -6,7 +6,7 @@
 /*   By: rkanmado <rkanmado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 03:01:21 by rkanmado          #+#    #+#             */
-/*   Updated: 2022/09/21 07:16:12 by rkanmado         ###   ########.fr       */
+/*   Updated: 2022/09/23 21:10:53 by rkanmado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,55 @@ void ft_sorting_process(t_push_swap **push_swap)
 {
     t_stack_bdle *stack_a;
     t_stack_bdle *stack_b;
+    t_stack_bdle stack_to_sort;
     int data;
+    int chunk;
+    int middle
 
     stack_a = &(*push_swap)->stack_a;
     stack_b = &(*push_swap)->stack_b;
-    while((*push_swap)->middle.index > 0)
+    middle = (*push_swap)->middle;
+    chunk = 0;
+
+    while (stack_a->head->chunk == chunk && stack_a->size > 1)
     {
-        while (stack_a->size > 1)
+        get_chunk_to_sort(&stack_to_sort, chunk, stack_a);
+        middle = get_middle_sorted(stack_to_sort);
+
+        while(stack_a->head->next != NULL)
         {
-            if (stack_a->head->data > (*push_swap)->middle.data)
+            if (stack_a->size == 2)
             {
-                data = ft_pop(&stack_a);
-                ft_take_top_x_to_top_y(&stack_a, &stack_b, "pa");
+                if (stack_a->head->data < stack_a->tail->data)
+                {
+                    stack_a->head->chunk = chunk;
+                    t_take_top_x_to_top_y(stack_a, stack_b, "pb");
+
+                }
+                else
+                {
+                    ft_swap_with_next_node(stack_a, "sa");
+                    stack_a->head->chunk = chunk;
+                    ft_take_top_x_to_top_y(stack_a, stack_b,"pb");
+                }
+                break;
             }
-        }
-        while (stack_a->size == 2)
-        {
-            if (stack_a->head->data > stack_b->tail->data)
+            while (stack_a->head->data > middle && stack_a->size > 1)
             {
-                ft_mvt_top_to_bottom(&stack_a, "ra");
-                ft_take_top_x_to_top_y(&stack_a, &stack_b, "pb");
+                data = ft_pop(stack_a);
+                ft_push(stack_b, data, chunk);
+            }
+            while (stack_a->head < middle && stack_a->size > 1)
+            {
+                while (stack_a->tail->data < middle)
+                {
+                    ft_mvt_bottom_to_top(stack_a, "rra");
+                    ft_take_top_x_to_top_y(&stack_a, &stack_b, "pb");
+                }
+                while (stack_a->tail->data > middle && stack_a->size > 1)
+                {
+                    ft_mvt_top_to_bottom(stack_a, "ra");
+                }
             }
         }
     }
